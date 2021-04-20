8890f249c2406dfeb8a70c5e35fa72269fe037a9
Juan C. Garcia <jgarcia@flourishsoftware.com>
2021-04-20 00:17:30 -0500

    refactoring for clarity

--- html.go
+++ html.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 	"html/template"
 	"image/color"
-	"log"
-	"os"
 	"path"
 	"strings"
 
@@ -15,8 +13,8 @@ import (
 )
 
 var (
-	// chessTemplate = "chessTemplate.html"
-	chessTemplate = os.Getenv("HOME") + "/chessTemplate.html"
+	chessTemplate = "chessTemplate.html"
+	// chessTemplate = os.Getenv("HOME") + "/chessTemplate.html"
 )
 
 type gamesHtml struct {
@@ -34,6 +32,44 @@ type gameHtml struct {
 	ID    string
 }
 
+func getGameHTML(g game) (gameHtml, error) {
+	whiteSplit := strings.Split(g.White, "/")
+	white := whiteSplit[len(whiteSplit)-1]
+
+	blackSplit := strings.Split(g.Black, "/")
+	black := blackSplit[len(blackSplit)-1]
+
+	// default mark to not have any markings
+	yellow := color.RGBA{255, 255, 0, 1}
+	mark := image.MarkSquares(yellow)
+
+	// if at least one move has been made, mark the last move
+	moves := g.ChessGame.Moves()
+	if len(moves) > 0 {
+		lastMove := moves[len(moves)-1]
+		mark = image.MarkSquares(yellow, lastMove.S1(), lastMove.S2())
+	}
+
+	svgBuffer := bytes.Buffer{}
+
+	// write board SVG to file
+	board := g.ChessGame.Position().Board()
+	if err := image.SVG(&svgBuffer, board, mark); err != nil {
+		return gameHtml{}, fmt.Errorf("could not get svg file: %w", err)
+	}
+
+	svgBytes := svgBuffer.Bytes()
+
+	svgBase64 := base64.StdEncoding.EncodeToString(svgBytes)
+
+	return gameHtml{
+		Black: black,
+		White: white,
+		Image: svgBase64,
+		ID:    g.ID,
+	}, nil
+}
+
 func getIndexHTML(games []game) ([]byte, error) {
 
 	htmlGames := make([]gameSlice, 0)
@@ -41,41 +77,12 @@ func getIndexHTML(games []game) ([]byte, error) {
 	currGameSlice := make([]gameHtml, 0)
 	for _, game := range games {
 
-		whiteSplit := strings.Split(game.White, "/")
-		white := whiteSplit[len(whiteSplit)-1]
-
-		blackSplit := strings.Split(game.Black, "/")
-		black := blackSplit[len(blackSplit)-1]
-
-		// default mark to not have any markings
-		yellow := color.RGBA{255, 255, 0, 1}
-		mark := image.MarkSquares(yellow)
-
-		// if at least one move has been made, mark the last move
-		moves := game.ChessGame.Moves()
-		if len(moves) > 0 {
-			lastMove := moves[len(moves)-1]
-			mark = image.MarkSquares(yellow, lastMove.S1(), lastMove.S2())
+		gHtml, err := getGameHTML(game)
+		if err != nil {
+			return nil, fmt.Errorf("could not get gameHtml: %w", err)
 		}
 
-		svgBuffer := bytes.Buffer{}
-
-		// write board SVG to file
-		board := game.ChessGame.Position().Board()
-		if err := image.SVG(&svgBuffer, board, mark); err != nil {
-			log.Fatal(err)
-		}
-
-		svgBytes := svgBuffer.Bytes()
-
-		svgBase64 := base64.StdEncoding.EncodeToString(svgBytes)
-
-		currGameSlice = append(currGameSlice, gameHtml{
-			Black: black,
-			White: white,
-			Image: svgBase64,
-			ID:    game.ID,
-		})
+		currGameSlice = append(currGameSlice, gHtml)
 		gamesInCurrentSlice++
 
 		if gamesInCurrentSlice == 3 {
@@ -106,15 +113,3 @@ func getIndexHTML(games []game) ([]byte, error) {
 
 	return outputParsed.Bytes(), nil
 }
-
-// coverName := path.Base(coverFilePath)
-// coverTmpl, err := template.New(coverName).Funcs(funcMap).ParseFiles(coverFilePath)
-// if err != nil {
-// 	return nil, fmt.Errorf("could not parse conver file template: %w", err)
-// }
-
-// coverOutputParsed := bytes.Buffer{}
-// err = coverTmpl.Execute(&coverOutputParsed, data)
-// if err != nil {
-// 	return nil, fmt.Errorf("could not execute cover file template: %w", err)
-// }
